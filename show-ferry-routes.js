5f1bb85c42ce75ecc12b54d0312847b164c47635
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-06-19 18:52:38 +0100

    Update show-ferry-routes.js

--- show-ferry-routes.js
+++ show-ferry-routes.js
@@ -1,1 +1,34 @@
+let ferryOperators = [];
+let ferryRoutes = [];
+let ferryTags = [];
+let activeTags = [];
+
+fetch('ferry-operators.json')
+  .then(response => response.json())
+  .then(data => {
+    ferryOperators = data;
+    console.log("Ferry Operators loaded:", ferryOperators);
+  })
+  .catch(error => console.error("Error loading ferry data:", error));
+
+fetch('ferry-routes.json')
+  .then(response => response.json())
+  .then(data => {
+    ferryRoutes = data;
+    console.log("Ferry Routes loaded:", ferryRoutes);
+     // ✅ Render routes after data is loaded and DOM is ready
+    renderAllRoutes();
+  })
+  .catch(error => console.error("Error loading ferry data:", error));
+
+fetch('ferry-tags.json')
+  .then(response => response.json())
+  .then(data => {
+    ferryTags = data;
+    console.log("Ferry Tags loaded:", ferryTags);
+    createTagButtons(); // load tag buttons
+  })
+  .catch(error => console.error("Error loading ferry data:", error));
+
+
 
